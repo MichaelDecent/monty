eb2613e8b88f4b0cdedd31a95dbc31cde5e83572
MichaelDecent <michaeldecent2@gmail.com>
2023-03-23 12:07:01 +0000

    Implement the push and pall opcodes.

--- handle_opscode.c
+++ handle_opscode.c
@@ -19,7 +19,7 @@ int handle_opcode(char *opcode, int line_number, stack_t **stack)
 		}
 		i++;
 	}
-	if (ops_array[i].opcode && opcode == NULL)
+	if (opcode && ops_array[i].opcode == NULL)
 		return (-1);
 	return (-1);
 }
--- monty_main.c
+++ monty_main.c
@@ -33,15 +33,19 @@ int main(int ac, char**av)
 
 	while ((getline(&line_content, &n, file)) != -1)
 	{
-		opcode = strtok(line_content, " \n\t");
-		oparg = strtok(NULL, " \n\t");
-		if (handle_opcode(opcode, line_number, &stack) == -1)
+		printf("line_content => %s", line_content);
+		if (line_content)
 		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-			fclose(file);
-			free(line_content);
-			free(stack);
-			exit(EXIT_FAILURE);
+			opcode = strtok(line_content, " \n\t");
+			oparg = strtok(NULL, " \n\t");
+			if (handle_opcode(opcode, line_number, &stack) == -1)
+			{
+				fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+				fclose(file);
+				free(line_content);
+				free(stack);
+				exit(EXIT_FAILURE);
+			}
 		}
 		line_number++;
 	}
