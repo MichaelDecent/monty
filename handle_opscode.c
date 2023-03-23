16e557d75dc791208905cdfdd228f19716ebb615
MichaelDecent <michaeldecent2@gmail.com>
2023-03-23 17:52:05 +0000

    Update Monty Program

--- handle_opscode.c
+++ handle_opscode.c
@@ -10,22 +10,15 @@
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t opst[] = {
-				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
-				{"pop", f_pop},
-				{"swap", f_swap},
-				{"add", f_add},
-				{"nop", f_nop},
-				{"sub", f_sub},
-				{"div", f_div},
-				{"mul", f_mul},
-				{"mod", f_mod},
-				{"pchar", f_pchar},
-				{"pstr", f_pstr},
-				{"rotl", f_rotl},
-				{"rotr", f_rotr},
-				{"queue", f_queue},
-				{"stack", f_stack},
-				{NULL, NULL}
+				{"push", f_push}, {"pall", f_pall},
+				{"pint", f_pint}, {"pop", f_pop},
+				{"swap", f_swap}, {"add", f_add},
+				{"nop", f_nop}, {"sub", f_sub},
+				{"div", f_div}, {"mul", f_mul},
+				{"mod", f_mod}, {"pchar", f_pchar},
+				{"pstr", f_pstr}, {"rotl", f_rotl},
+				{"rotr", f_rotr}, {"queue", f_queue},
+				{"stack", f_stack}, {NULL, NULL}
 				};
 	unsigned int i = 0;
 	char *op;
